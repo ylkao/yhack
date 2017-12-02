db3ff9d175c2d154f1e8b2a22ab1b1f5b1d82f95
Yiling Kao <ylkao@berkeley.edu>
2017-12-02 13:57:36 -0800

    fixing merge

--- fileUpload.py
+++ fileUpload.py
@@ -17,25 +17,6 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-<<<<<<< HEAD
-        if request.form['submit'] == 'upload':
-            # check if the post request has the file part
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            # if user does not select file, browser also
-            # submit a empty part without filename
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                # return redirect(url_for('uploaded_file', filename=filename))
-        elif request.form['submit'] == 'process':
-            return 'processing form'
-=======
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
@@ -60,7 +41,6 @@ def upload_file():
          <input type=submit value=Upload>
     </form>
     '''"""
->>>>>>> 4f44a91b65a787c1730fcc4776da17f28bd79a43
     return render_template('index.html')
 
 @app.route('/uploads/<filename>')
