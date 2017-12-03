34eba002c10335b7d6cb453c50943471bfee25b4
Yiling Kao <ylkao@berkeley.edu>
2017-12-03 01:12:55 -0800

    somewhat finished coloring and styling output

--- fileUpload.py
+++ fileUpload.py
@@ -14,6 +14,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route("/index")
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     anger = []
